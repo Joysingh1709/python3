b9321cdc7cdfef44d7ee07f6e15d63a69295dee2
Joy Singh <avnishmamta060606@gmail.com>
2020-01-13 12:56:55 +0530

    Add files via upload

---
+++ greatest3.py
@@ -0,0 +1,14 @@
+a=int(input("enter value of a : "))
+b=int(input("enter value of b : "))
+c=int(input("enter value of c : "))
+
+if a>b:
+    if a>c:
+        print(a," is greater")
+if b>a:
+    if b>c:
+        print(b," is greater")
+if c>a:
+    if c>b:
+        print(c," is greater")
+
---
+++ swap1.py
@@ -0,0 +1,9 @@
+a=int(input("enter a : "))
+b=int(input("enter b : "))
+
+c=a
+a=b
+b=c
+
+print(a)
+print(b)
